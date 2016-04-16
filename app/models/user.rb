60ad8ec57b07ee84179e85625ace91d17a0c9f9c
Zzorin <ilya-zorin@yandex.ru>
2016-04-16 23:07:38 +0600

    Add microposts/fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
   private
 
     def create_remember_token
-      self.remember_token = User.digist(User.new_remember_token)
+      self.remember_token = User.encrypt(User.new_remember_token)
     end
 
     
